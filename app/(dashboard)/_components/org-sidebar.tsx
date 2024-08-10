1c192581cdb5ef1dfbe82c9b98a578f428142d22
minhttran <trcongminh.261002@gmail.com>
2024-08-10 13:58:38 +0700

    Dashboard layout

---
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const OrgSidebar = () => {
+  return (
+    <div className="hidden lg:flex flex-col space-y-6 pl-5 w-[260px] pt-5 bg-red-400">
+      org
+    </div>
+  );
+};
+
+export default OrgSidebar;
